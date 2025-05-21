36f38080e78b43c9a333d74cb9d8e66cbdf16d68
Yokumii <KemingZhang@bupt.edu.cn>
2025-05-21 08:58:51 +0800

    fix: 修复win系统下的兼容性问题

--- QRSignSimulator/core/clipboard.py
+++ QRSignSimulator/core/clipboard.py
@@ -3,7 +3,7 @@
 负责从剪贴板获取图像数据
 """
 
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 import numpy as np
 import cv2
 
@@ -23,6 +23,19 @@ class ClipboardManager:
             clipboard_img = ImageGrab.grabclipboard()
             
             if clipboard_img is None:
+                return None, None
+            
+            # Windows系统下，clipboard_img可能是图片路径列表
+            if isinstance(clipboard_img, list):
+                if len(clipboard_img) > 0:
+                    # 取第一个图片路径
+                    img_path = clipboard_img[0]
+                    clipboard_img = Image.open(img_path)
+                else:
+                    return None, None
+            
+            # 确保clipboard_img是PIL.Image对象
+            if not isinstance(clipboard_img, Image.Image):
                 return None, None
             
             # 转换为NumPy数组
